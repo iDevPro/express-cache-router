2a485078260fe168ef1d4b1861f17a7e4d722c40
iQQator <itux@idev.pro>
2016-01-06 00:03:12 +0300

    Extend functionality, add id for check if request has another id router.use('users/:id', cache());

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ function CacheMiddleware(options) {
     options = options || {};
     var expire = options.expire || 5 * 60 * 1000;
     var content;
+    var id; // id from request, you can add another parameters for test, object can't be equal
 
     return function(req, res, next) {
-        if(content) {
+        //  if content present and id equal params.id (undefined is equal undefined)
+        if(content && id == req.params.id) { 
             res.charset = 'utf-8';
             res.contentType('application/json; charset=UTF-8');
             return res.end(content);
@@ -13,6 +15,7 @@ function CacheMiddleware(options) {
         res.end = function(buffer) {
             if(req.method === 'GET' && (typeof buffer === 'string' || Buffer.isBuffer(buffer)) ) {
                 content = buffer;
+                id = req.params.id; // get id from params here
                 setTimeout(function() {
                     content = null;
                 }, expire);
