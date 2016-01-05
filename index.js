9cc2687609bcb462a6436f911d5dcebd91cfa403
iQQator <itux@idev.pro>
2016-01-05 22:59:28 +0300

    Fix again :) But it's work now like a charm :)

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ function CacheMiddleware(options) {
 
     return function(req, res, next) {
         if(content) {
+            res.charset = 'utf-8';
+            res.contentType('application/json; charset=UTF-8');
             return res.end(content);
         }
         var end = res.end;
         res.end = function(buffer) {
             if(req.method === 'GET' && (typeof buffer === 'string' || Buffer.isBuffer(buffer)) ) {
-                content = buffer.toString('utf8');
+                content = buffer;
                 setTimeout(function() {
                     content = null;
                 }, expire);
